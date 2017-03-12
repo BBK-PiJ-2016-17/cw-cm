5cfbd3a4031bf3bd68507723768a0c0ac7a247b3
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-03-12 20:23:03 +0000

    Fixed MeetingImpl to be Abstract class and fixed PastMeetings

--- src/main/java/impl/MeetingImpl.java
+++ src/main/java/impl/MeetingImpl.java
@@ -11,20 +11,32 @@ import java.util.Set;
  * @author Ginestra Ferraro
  */
 
-public class MeetingImpl implements Meeting {
+public abstract class MeetingImpl implements Meeting {
     private int id;
     private Calendar date;
     private Set<Contact> contacts;
 
+    public MeetingImpl(int id, Calendar date, Set<Contact> contacts) {
+        if (date == null || contacts == null) {
+            throw new NullPointerException();
+        } else if (id <= 0 || contacts.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.id = id;
+        this.date = date;
+        this.contacts = contacts;
+    }
+
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public Calendar getDate() {
-        return this.date;
+        return date;
     }
 
     public Set<Contact> getContacts() {
-        return this.contacts;
+        return contacts;
     }
 }
--- src/main/java/impl/PastMeetingImpl.java
+++ src/main/java/impl/PastMeetingImpl.java
@@ -1,11 +1,33 @@
 package impl;
 
+import spec.Contact;
 import spec.PastMeeting;
+
+import java.util.Calendar;
+import java.util.Set;
 
 /**
  * @author Ginestra Ferraro
  */
 
-public class PastMeetingImpl implements PastMeeting {
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 
+    private String notes;
+
+    public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts,
+                           String notes) {
+
+        super(id, date, contacts);
+
+        // Here the only check i need is for the notes
+        if (notes == null) {
+            throw new NullPointerException("ERROR: The notes can't be null");
+        }
+
+        this.notes = notes;
+    }
+
+    public String getNotes() {
+        return this.notes;
+    }
 }
