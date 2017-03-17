a18158699c99094cc64ac82809367fa15ab9f9ac
BBK-PiJ-2016-17 <gferra02@mail.bbk.ac.uk>
2017-03-17 10:53:40 +0000

    Adding more tests, but failed to test getContacts() in the abstract class

--- src/test/java/test/MeetingTest.java
+++ src/test/java/test/MeetingTest.java
@@ -27,7 +27,7 @@ public class MeetingTest {
     private Calendar date;
     private Set<Contact> contacts;
 
-    private Meeting meeting;
+    private Meeting meeting, anotherMeeting;
 
     @Before
     public void mockMeeting() {
@@ -40,12 +40,50 @@ public class MeetingTest {
         meeting = new FutureMeetingImpl(2, date, contacts);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void isItNull() {
+        System.out.println("==== Testing constructor for NPE");
+        meeting = new FutureMeetingImpl(3, null, contacts);
+        anotherMeeting = new FutureMeetingImpl(4, date, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void isItLegal() {
+        System.out.println("==== Testing constructor for IAE / Negative id");
+        meeting = new FutureMeetingImpl(-2, date, contacts);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void isAnyoneIn() {
+        System.out.println("==== Testing constructor for IAE / Empty contacts");
+        contacts = null;
+        contacts = new HashSet<Contact>();
+        meeting = new FutureMeetingImpl(13, date, contacts);
+    }
+
     @Test
     public void getIdTest() {
         System.out.println("==== Testing the getId() method");
         int getTheId = meeting.getId();
         int expected = 2;
         assertEquals(expected, getTheId);
+    }
+
+    @Test
+    public void getDateTest() {
+        System.out.println("==== Testing the getDate() method");
+        date = null;
+        date = Calendar.getInstance();
+        meeting = new FutureMeetingImpl(13, date, contacts);
+        Calendar getTheDate = meeting.getDate();
+        Calendar expected = date;
+        assertEquals(expected, getTheDate);
+    }
+
+    @Test
+    public void getContactsTest() {
+        System.out.println("==== Testing the getContacts() method");
+        // How do I test that the hash is the same?
     }
 
     @After
